d0e70f1fa13bb7634933350df5864d381fd6055f
ThaminduNR <thaminduniwantha777@gmail.com>
2024-10-03 08:10:02 +0530

    add the project

---
+++ App.jsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App(){
+
+  return (
+    <View>
+      <Text className="text-2xl text-black mt-4">
+        Hello, World!
+      </Text>
+    </View>
+   
+  );
+}
+
+
+
+export default App;
