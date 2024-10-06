ae3912f0076ad672acfc34bb88cc5970490f26b8
ThaminduNR <thaminduniwantha777@gmail.com>
2024-10-06 13:27:23 +0530

    fix cart button

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -2,6 +2,7 @@ import {View, Text, SafeAreaView, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import InnerHeader from '../components/InnerHeader';
 import {useRoute} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const ProductScreen = () => {
   const {params: item} = useRoute();
@@ -9,7 +10,7 @@ const ProductScreen = () => {
   return (
     <SafeAreaView className="flex-1 justify-between bg-white">
       <InnerHeader label="Product Details" />
-      <View className=" mx-4">
+      <View className=" mx-4 flex-2">
         <View className="w-[100%] h-[320] bg-slate-200 rounded-md mb-3">
           <Image
             source={{uri: item?.item?.mainImage}}
@@ -32,7 +33,7 @@ const ProductScreen = () => {
             style={{fontFamily: 'Poppins-Bold'}}>
             {item?.item?.price.currency} {item?.item?.price.amount}
           </Text>
-          <View className="flex-row justify-between mt-3">
+          <View className="flex-row justify-between mt-2">
             <Text
               className="text-md text-black "
               style={{fontFamily: 'Poppins-Regular'}}>
@@ -44,8 +45,12 @@ const ProductScreen = () => {
               Stock : {item?.item?.stockStatus}
             </Text>
           </View>
-          <View className="mt-2">
-            <Text className="text-black">Available Size</Text>
+          <View>
+            <Text
+              className="text-black"
+              style={{fontFamily: 'Poppins-Regular'}}>
+              Available Size
+            </Text>
             <View className="flex-row mt-1 ">
               {item?.item?.sizes.map((size, index) => (
                 <View className="w-[30] h-[30] rounded-full bg-slate-400 items-center justify-center mx-1">
@@ -54,8 +59,12 @@ const ProductScreen = () => {
               ))}
             </View>
           </View>
-          <View className="mt-4">
-            <Text className="text-black">Colors</Text>
+          <View className="mt-1">
+            <Text
+              className="text-black"
+              style={{fontFamily: 'Poppins-Regular'}}>
+              Colors
+            </Text>
             <View className="flex-row mt-1">
               <View
                 className="w-[20] h-[20] rounded-full items-center justify-center mx-1"
@@ -64,21 +73,27 @@ const ProductScreen = () => {
           </View>
         </View>
       </View>
-      <View className=" mx-4   mb-4">
-        <View className="w-[100%] h-[100] overflow-hidden">
+      <View className=" mx-4 flex-1 justify-between ">
+        <View className="w-[100%] h-auto flex-1 ">
           <Text
             className="  text-black"
             style={{fontFamily: 'Poppins-Italic', fontSize: 12}}>
             {item?.item?.description}
           </Text>
         </View>
-        <TouchableOpacity className="w-[85%] h-[55] bg-black rounded-3xl mt-4  items-center justify-center">
-          <Text
-            className="text-white text-lg"
-            style={{fontFamily: 'Poppins-Bold'}}>
-            Add to Cart
-          </Text>
-        </TouchableOpacity>
+        <View className="flex-row  flex-1 justify-between top-4">
+          <TouchableOpacity className="w-[85%] h-[55] bg-black rounded-3xl items-center justify-center">
+            <Text
+              className="text-white text-lg"
+              style={{fontFamily: 'Poppins-Bold'}}>
+              Add to Cart
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity className="rounded-lg bg-white h-[55] w-[13%] items-center justify-center shadow-xl">
+            <Icon size={35} color={'black'} name="cart-outline" />
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
