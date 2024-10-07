891b323a829897532302830460802eeec68d6c22
ThaminduNR <thaminduniwantha777@gmail.com>
2024-10-07 21:33:10 +0530

    cart fix

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -6,16 +6,35 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import InnerHeader from '../components/InnerHeader';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {removeFromCart} from '../redux/cartSlice';
 
 const CartScreen = () => {
   const cartItems = useSelector(state => state.cart.items);
-  const renderItem = ({item}) => {
+  const [cart, setCart] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setCart(cartItems);
+    console.log('Cart Items', cart);
+  }, [cartItems, cart]);
+
+  const removeItem = itemId => {
+    dispatch(removeFromCart(itemId));
+  };
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + (item.price?.amount || 0);
+  }, 0);
+
+  const renderItem = ({item}, index) => {
     return (
-      <View className="flex-row h-[130] gap-x-3 mb-4 pb-1 border-gray-300 border-b-2">
+      <View
+        className="flex-row h-[130] gap-x-3 mb-4 pb-1 border-gray-300 border-b-2"
+        key={index}>
         <Image
           source={{uri: item.item.mainImage}}
           style={{
@@ -43,7 +62,9 @@ const CartScreen = () => {
             <Text style={{fontFamily: 'Poppins-Regular', color: 'black'}}>
               Quantity : 1
             </Text>
-            <TouchableOpacity className="p-1 bg-slate-300 rounded-full">
+            <TouchableOpacity
+              className="p-1 bg-slate-300 rounded-full"
+              onPress={() => removeItem(item.item.SKU)}>
               <Icon size={22} color={'black'} name="trash-outline" />
             </TouchableOpacity>
           </View>
@@ -57,8 +78,8 @@ const CartScreen = () => {
       <InnerHeader label="My Cart" iconName="ellipsis-horizontal-sharp" />
       <View style={{flex: 4, marginHorizontal: 16, marginTop: 16}}>
         <FlatList
-          data={cartItems}
-          keyExtractor={item => item.id}
+          data={cart}
+          keyExtractor={(item, index) => item.id}
           renderItem={renderItem}
         />
       </View>
@@ -84,7 +105,9 @@ const CartScreen = () => {
               color: 'black',
               fontSize: 20,
             }}>
-            $ 24.00
+            {cart.length > 0
+              ? `${cart[0].item.price?.currency} ${totalPrice.toFixed(2)}`
+              : 'N/A'}
           </Text>
         </View>
         <TouchableOpacity
